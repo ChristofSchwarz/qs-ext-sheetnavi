80a8b16f4bf86f21702c4b193ea5a68b398e8aa1
Christof Schwarz <christof.schwarz@databridge.ch>
2022-11-17 14:53:14 +0100

    Delete gitpush.bat

--- gitpush.bat
+++
@@ -1,8 +0,0 @@
-@REM First time:
-@REM git clone https://github.com/ChristofSchwarz/qs-ext-sheetnavi
-@REM git init
-@REM git remote add origin git@github.com:ChristofSchwarz/qs-ext-sheetnavi.git
-
-git add .
-git commit -m "updates"
-git push -u origin main
